932d57dde58854345c22b1327f0911cb96201f69
lolopop22 <59579270+lolopop22@users.noreply.github.com>
2021-03-20 22:53:55 +0100

    Delete src/main/java/org/loic/api_soap_team directory

--- src/main/java/org/loic/api_soap_team/dao/PlayerRepository.java
+++
@@ -1,8 +0,0 @@
-package org.loic.api_soap_team.dao;
-
-import org.loic.api_soap_team.domains.Player;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlayerRepository extends JpaRepository<Player, Long> {
-
-}
--- src/main/java/org/loic/api_soap_team/dao/TeamRepository.java
+++
@@ -1,8 +0,0 @@
-package org.loic.api_soap_team.dao;
-
-import org.loic.api_soap_team.domains.TeamEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TeamRepository extends JpaRepository<TeamEntity, Long> {
-
-}
